29739e0b79f7ab0d08a7859ce9d9a34a01ec0eb0
John Helveston <john.helveston@gmail.com>
2023-01-12 00:16:20 -0500

    formatting data types

---
+++ code/explore.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+
+df <- arrow::read_parquet(file.path('data-raw', 'data.parquet'))
+
+df %>%
+    filter(body_style == 'Pickup Truck') %>%
+    group_by(year) %>%
+    summarise(median(vehicle_height_in, na.rm = TRUE)) %>%
+    data.frame()
+
+df %>%
+    filter(body_style == 'Pickup Truck') %>%
+    filter(vehicle_height_in < 90) %>%
+    group_by(year) %>%
+    ggplot() +
+    geom_boxplot(
+        aes(x = year, y = vehicle_height_in, group = year)
+    ) +
+    theme_bw() +
+    labs(
+        x = "Year",
+        y = "Vehicle height (inches)"
+    )
+
+ggsave('pickup_height.png', width = 7, height = 5)
--- code/parse-html.R
+++ code/parse-html.R
@@ -21,4 +21,41 @@ for (i in 1:length(pages)) {
 
 df <- do.call(rbind, data)
 
+# Fix formatting
+
+df <- df %>%
+    mutate(
+        msrp                      = parse_number(msrp),
+        invoice_price             = parse_number(invoice_price),
+        used_new_price            = parse_number(used_new_price),
+        vehicle_length_in         = parse_number(vehicle_length),
+        vehicle_width_in          = parse_number(vehicle_width),
+        vehicle_height_in         = parse_number(vehicle_height),
+        range_mi                  = parse_number(range),
+        electric_range_mi         = parse_number(electric_range),
+        wheelbase_in              = parse_number(wheelbase),
+        ground_clearance_in       = parse_number(ground_clearance),
+        power_to_weight_ratio_in  = parse_number(power_to_weight_ratio),
+        cargo_capacity_in         = parse_number(cargo_capacity),
+        front_headroom_in         = parse_number(front_legroom),
+        front_legroom_in          = parse_number(front_legroom),
+        front_shoulder_room_in    = parse_number(front_shoulder_room),
+        front_hip_room_in         = parse_number(front_hip_room),
+        rear_headroom_in          = parse_number(rear_headroom),
+        rear_legroom_in           = parse_number(rear_legroom),
+        highway_fuel_economy_mpg  = parse_number(highway_fuel_economy),
+        city_fuel_economy_mpg     = parse_number(city_fuel_economy),
+        combined_fuel_economy_mpg = parse_number(combined_fuel_economy),
+        fuel_capacity_mpg         = parse_number(fuel_capacity)
+    ) %>%
+    select(-c(
+        'msrp', 'invoice_price', 'used_new_price', 'vehicle_length',
+        'vehicle_width', 'vehicle_height', 'range_mi',
+        'electric_range_mi', 'wheelbase', 'ground_clearance',
+        'power_to_weight_ratio', 'cargo_capacity', 'front_headroom',
+        'front_legroom', 'front_shoulder_room', 'front_hip_room',
+        'rear_headroom', 'rear_legroom', 'highway_fuel_economy',
+        'city_fuel_economy', 'combined_fuel_economy', 'fuel_capacity',
+    ))
+
 arrow::write_parquet(df, file.path('data-raw', 'data.parquet'))
